37093b47a8bffaa275f2c273d2c18ace03263b95
TigerLau <406447623@qq.com>
2015-03-17 17:16:04 +0800

    Cloning something from MyMap

--- MapTest/gen/com/tigerlau/maptest/R.java
+++ MapTest/gen/com/tigerlau/maptest/R.java
@@ -11,17 +11,65 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int common_icon_back_btn_normal=0x7f020000;
+        public static final int common_icon_back_btn_pressed=0x7f020001;
+        public static final int common_icon_back_btn_selector=0x7f020002;
+        public static final int common_icon_searchbox_magnifier_2=0x7f020003;
+        public static final int common_icon_searchbox_voice_selector=0x7f020004;
+        public static final int common_searchbox_home_bg=0x7f020005;
+        public static final int distance_line=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int main_bl_button_background=0x7f020008;
+        public static final int main_bl_button_down=0x7f020009;
+        public static final int main_bm_button_background=0x7f02000a;
+        public static final int main_bm_button_down=0x7f02000b;
+        public static final int main_bottombtn_down=0x7f02000c;
+        public static final int main_bottombtn_up=0x7f02000d;
+        public static final int main_br_button_background=0x7f02000e;
+        public static final int main_br_button_down=0x7f02000f;
+        public static final int main_icon_route=0x7f020010;
+        public static final int main_icon_searchbox=0x7f020011;
+        public static final int main_icon_voice=0x7f020012;
+        public static final int main_icon_zoomin=0x7f020013;
+        public static final int main_icon_zoomin_dis=0x7f020014;
+        public static final int main_icon_zoomout=0x7f020015;
+        public static final int main_icon_zoomout_dis=0x7f020016;
+        public static final int main_locatebtn_down=0x7f020017;
+        public static final int main_locatebtn_up=0x7f020018;
+        public static final int main_locatebutton=0x7f020019;
+        public static final int main_searchbox_voice_selector=0x7f02001a;
+        public static final int main_topbtn_down=0x7f02001b;
+        public static final int main_topbtn_up=0x7f02001c;
+        public static final int search_box=0x7f02001d;
+        public static final int zoomin_seletor=0x7f02001e;
+        public static final int zoomout_seletor=0x7f02001f;
     }
     public static final class id {
-        public static final int bmapview_container=0x7f060000;
+        public static final int back_btn=0x7f060000;
+        public static final int bmapview_container=0x7f060003;
+        public static final int common_search_edit=0x7f060002;
+        public static final int common_speak_btn=0x7f060001;
+        public static final int searchedit=0x7f060005;
+        public static final int speak_btn=0x7f060004;
+        public static final int zoomin=0x7f060006;
+        public static final int zoomout=0x7f060007;
     }
     public static final class layout {
-        public static final int activity_launcher=0x7f030000;
+        public static final int activity_common_search=0x7f030000;
+        public static final int activity_launcher=0x7f030001;
+        public static final int common_searchbox_home=0x7f030002;
+        public static final int zoom_controls_layout=0x7f030003;
     }
     public static final class string {
+        public static final int action_settings=0x7f040001;
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int common_search_hint=0x7f040003;
+        public static final int mine=0x7f040005;
+        public static final int model_control=0x7f040004;
+        public static final int nav=0x7f040006;
+        public static final int nearby=0x7f040008;
+        public static final int route=0x7f040007;
+        public static final int search_hint=0x7f040002;
     }
     public static final class style {
         /** 
