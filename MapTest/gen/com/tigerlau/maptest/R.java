4baaf71c211de080e7b198a2425c748596e13204
TigerLau <406447623@qq.com>
2015-03-18 16:46:15 +0800

    Navigation Layout improved.

--- MapTest/gen/com/tigerlau/maptest/R.java
+++ MapTest/gen/com/tigerlau/maptest/R.java
@@ -72,10 +72,11 @@ containing a value of this type.
     public static final class layout {
         public static final int activity_common_search=0x7f030000;
         public static final int activity_launcher=0x7f030001;
-        public static final int common_searchbox_home=0x7f030002;
-        public static final int navigation=0x7f030003;
-        public static final int test=0x7f030004;
-        public static final int zoom_controls_layout=0x7f030005;
+        public static final int common_navigation_home=0x7f030002;
+        public static final int common_navigation_home2=0x7f030003;
+        public static final int common_searchbox_home=0x7f030004;
+        public static final int test=0x7f030005;
+        public static final int zoom_controls_layout=0x7f030006;
     }
     public static final class string {
         public static final int action_settings=0x7f040001;
